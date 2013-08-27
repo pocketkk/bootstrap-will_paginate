7348417a26cb4cd6865ded7f1fb9c265eb9380d0
Jason Crump <pocketkk@gmail.com>
2013-08-27 14:42:25 -0700

    changed to small pagination

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -1,5 +1,5 @@
 # config/initializers/will_paginate.rb
-# 
+#
 # This extension code was written by Isaac Bowen, originally found
 # at http://isaacbowen.com/blog/using-will_paginate-action_view-and-bootstrap/
 
@@ -10,16 +10,16 @@ module WillPaginate
     def will_paginate(collection = nil, options = {})
       options, collection = collection, nil if collection.is_a? Hash
       # Taken from original will_paginate code to handle if the helper is not passed a collection object.
-      collection ||= infer_collection_from_controller 
+      collection ||= infer_collection_from_controller
       options[:renderer] ||= BootstrapLinkRenderer
       super.try :html_safe
     end
 
     class BootstrapLinkRenderer < LinkRenderer
       protected
-      
+
       def html_container(html)
-        tag :div, tag(:ul, html, :class => "pagination"), container_attributes
+        tag :div, tag(:ul, html, :class => "pagination pagination-small"), container_attributes
       end
 
       def page_number(page)
